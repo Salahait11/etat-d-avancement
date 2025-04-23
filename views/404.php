c7fa0f43a1eb09970e4b656bfbe20806ebc3ab98
Salah Ait hammou <aithammousalah0@gmail.com>
2025-04-23 21:42:38 +0100

    z

--- views/404.php
+++
@@ -1,6 +0,0 @@
-<div class="text-center mt-5">
-    <h1 class="display-1 text-danger">404</h1>
-    <h2 class="mb-4">Page non trouvée</h2>
-    <p class="lead mb-4">La page que vous recherchez n'existe pas ou a été déplacée.</p>
-    <a href="/" class="btn btn-primary">Retour à l'accueil</a>
-</div> 
